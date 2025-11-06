c97689c613eb0b38a6fee52eedc0919470b05d6d
shavits <s.shavit4@gmail.com>
2025-11-06 11:29:47 +0200

    added hits middleware

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
-func main(){
 
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
+
+func main(){
+	apiCfg := apiConfig{}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app",http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.HandleFunc("/metrics", apiCfg.handlerHits)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	server := &http.Server{
-		Handler: serveMux,
+		Handler: mux,
 		Addr: ":8080",
 	}
 
@@ -18,4 +28,45 @@ func main(){
     if err := server.ListenAndServe(); err != nil {
         log.Fatal(err)
     }
+}
+
+func handlerHealthz(resWriter http.ResponseWriter,req *http.Request){
+	resWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+
+	_, err := resWriter.Write([]byte("OK"))
+	if err != nil{
+		log.Println(err)
+	}
+}
+
+func (apiCfg *apiConfig) handlerHits(resWriter http.ResponseWriter,req *http.Request){
+	resWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+
+
+    hitsStr := "Hits: " + strconv.Itoa(int(apiCfg.fileserverHits.Load()))
+    _, err := resWriter.Write([]byte(hitsStr))
+    if err != nil {
+        log.Println(err)
+    }
+}
+
+func (apiCfg *apiConfig) handlerReset(resWriter http.ResponseWriter,req *http.Request){
+	resWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resWriter.WriteHeader(200)
+
+	apiCfg.fileserverHits.Store(0)
+    hitsStr := "Hits: " + strconv.Itoa(int(apiCfg.fileserverHits.Load()))
+    _, err := resWriter.Write([]byte(hitsStr))
+    if err != nil {
+        log.Println(err)
+    }
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
 }
